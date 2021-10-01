4646394f0e3ac51750d66df048b16a6986e75a91
SamuelGlezMrt <71700577+SamuelGlezMrt@users.noreply.github.com>
2021-10-01 12:10:22 +0100

    Update Main.java

--- src/main/java/dad/maven/gson/Main.java
+++ src/main/java/dad/maven/gson/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-
 public class Main {
 
 	public static void main(String[] args) {
@@ -15,20 +14,20 @@ public class Main {
 
 		System.out.println("[Campo obligatorio] Introduce un nombre:");
 		p.Nombre = scanner.nextLine();
-		
+
 		while (!p.Nombre.matches("^[a-zA-Z]+$")) {
 			System.out.println("Este nombre no es correcto, introduzca otro nombre:");
 			p.Nombre = scanner.nextLine();
 		}
-				
+
 		System.out.println("[Campo obligatorio] Introduce los apellidos:");
 		p.Apellidos = scanner.nextLine();
-		
+
 		while (!p.Apellidos.matches("^[a-zA-Z]+( [a-zA-Z]+)*$")) {
 			System.out.println("Los apellidos no son correctos, introduzca otros apellidos");
-		 	p.Apellidos = scanner.nextLine();
-		}		
-		
+			p.Apellidos = scanner.nextLine();
+		}
+
 		System.out.println("[Campo obligatorio] Introduce la edad:");
 		p.Edad = scanner.nextInt();
 
@@ -38,5 +37,5 @@ public class Main {
 		String json = gson.toJson(p);
 		System.out.println(json);
 	}
- 	
+
 }
